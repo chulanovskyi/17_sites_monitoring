8e4323a3dee4459ef65cadb379d39c6541004315
chulanovskyi <skyrocker@mail.ru>
2016-10-13 12:33:40 +0300

    task finish

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
 # 17_sites_monitoring
+
+###Prerequisites:
+
+Run in console `pip install -r requirements.txt` to install 3rd party modules.
+
+---
+
+Script will help you to check if site is alive (HTTP status code 200) 
+and its prepaid domain name expiration date more than 1 month.
+
+###How to use:
+
+Script takes one required argument:
+- `path` - path to text file with domain names;
+
+```**Note!** In text file domain names should be separated with spaces or new lines.```
+* *Good structure:* * devman.org github.com vk.com
+* *Good structure:* *
+devman.org
+github.com
+vk.com
+**_Not valid structure_**: devman.org; http://github.com | vk.com
+
+###Example:
+
+**urls.txt** data:
+devman.org
+github.com
+vk.com
+
+##INPUT
+`python check_site_health.py urls.txt`
+
+##OUPUT
+```
+--------------------
+devman.org
+Is alive: True
+Expire soon: False
+--------------------
+github.com
+Is alive: True
+Expire soon: False
+--------------------
+vk.com
+Is alive: True
+Expire soon: False
+```
--- check_sites_health.py
+++ check_sites_health.py
@@ -1,11 +1,48 @@
+import requests
+import argparse
+import datetime
+from whois import whois
+
+
+EXPIRE_SOON_DAYS = 30
+
+
+def create_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path')
+    return parser
+
+
 def load_urls4check(path):
-    pass
+    with open(path, encoding='utf-8') as url_file:
+        return url_file.read()
+
 
 def is_server_respond_with_200(url):
-    pass
+    response = requests.get('http://%s' % url)
+    return response.status_code == 200
 
-def get_domain_expiration_date(domain_name):
-    pass
+
+def is_domain_expire_soon(domain_name):
+    domain_info = whois(domain_name)
+    try:
+        relative, precise = domain_info.expiration_date
+        expire_date = relative
+    except TypeError:
+        expire_date = domain_info.expiration_date
+    if not expire_date:
+        return
+    today = datetime.date.today()
+    expire_delta = today + datetime.timedelta(days=EXPIRE_SOON_DAYS)
+    return expire_delta >= expire_date.date()
+
 
 if __name__ == '__main__':
-    pass
+    parser = create_parser()
+    path = parser.parse_args().path
+    urls = load_urls4check(path)
+    for url in urls.split():
+        print('-'*20)
+        print(url)
+        print('Is alive: %s' % is_server_respond_with_200(url))
+        print('Expire soon: %s' % is_domain_expire_soon(url))
